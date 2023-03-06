1d4e49aa67402849c5ef8fb200044f63d9c91536
Сергей Самойленко <corazonsergey@gmail.com>
2023-03-06 20:12:06 +0300

    Добавлена jwt-аутентификация

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route("/api", name: "api_")]
+class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'dashboard', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/DashboardController.php',
+        ]);
+    }
+}
