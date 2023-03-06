6cd5eaa4084008fce4cc8e22aedc8eae43d5f8a8
Alexander Androsov <alexandr.v.androsov@gmail.com>
2023-03-06 16:30:47 +0300

    first route and test

---
+++ tests/Fuctional/Controller/TestCheckActionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Fuctional\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class TestCheckActionTest extends WebTestCase
+{
+    public function testRequestSuccess()
+    {
+        $client = static::createClient();
+        $client->request(Request::METHOD_GET, '/test-check');
+
+        $this->assertResponseIsSuccessful();
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertEquals('ok', $result['status']);
+    }
+}
